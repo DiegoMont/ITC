c55d6f37ef1cd6d30cf5f583fa7af4c970430397
Diego Montaño <diegomonmar@hotmail.com>
2018-08-27 22:44:16 -0500

    Calculo de porcentaje completo

--- SemesterProgress.java
+++ SemesterProgress.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
 class SemesterProgress{
+  public static void main(String[] args) {
+    int numSu = 6; int numSe = 9; int numUn = 8;
+    Scanner s = new Scanner(System.in);
+    System.out.println("How many semesters have you completed?");
+    int compS = s.nextInt();
+    System.out.println("How many subjects have you failed?");
+    int failS = s.nextInt();
+    double totalU = numSu * numSe * numUn;
+    System.out.println(totalU);
+    double percent = (totalU - compS*numUn*numSu + failS*numUn) / totalU * 100;
+    System.out.println("You are still missing " + percent + "% of your career.");
+  }
 }

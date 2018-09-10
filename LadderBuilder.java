8158c361b7f7c0272e3ce4f5a7698cb1184ca5f0
Diego Montaño <diegomonmar@hotmail.com>
2018-09-10 17:02:33 -0500

    Fix erros with the conditions

--- LadderBuilder.java
+++ LadderBuilder.java
@@ -8,7 +8,7 @@ class LadderBuilder{
     int piezaP = consola.nextInt();
     System.out.print("Requested size of the ladder: ");
     int goalSize = consola.nextInt();
-    if (goalSize <= piezaP || goalSize/5 >= piezaG && piezaP >= goalSize%5 ) {
+    if (goalSize/5<=piezaG && goalSize%5 <= piezaP || goalSize-piezaG*5 <= piezaP && goalSize-piezaG*5 +piezaP >=0) {
       System.out.println("You can build it.");
       System.out.println("And you have "+(piezaG-goalSize/5<0 ? 0: piezaG-goalSize/5)+" big pieces left and "+(piezaP-(goalSize-(goalSize/5>=piezaG?piezaG:goalSize/5)*5))+" small pieces left.");
     } else {

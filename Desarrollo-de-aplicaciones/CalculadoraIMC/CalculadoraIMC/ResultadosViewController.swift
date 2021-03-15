9584ebd4274b8e00625ebecb561ead73a289cf8c
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-03-15 12:54:24 -0600

    App IMC dos pantallas

--- Desarrollo-de-aplicaciones/CalculadoraIMC/CalculadoraIMC/ResultadosViewController.swift
+++ Desarrollo-de-aplicaciones/CalculadoraIMC/CalculadoraIMC/ResultadosViewController.swift
@@ -11,22 +11,42 @@ class ResultadosViewController: UIViewController {
 
   @IBOutlet weak var IMCNumerico: UILabel!
   @IBOutlet weak var IMCTextual: UILabel!
+  @IBOutlet weak var imagen: UIImageView!
+  
+  var peso = 1.0, altura = 1.0;
   
   override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    let imc = peso / altura / altura;
+    IMCNumerico.text = String(format: "%.02f", imc);
+    switch imc {
+      case 0...18.5:
+        IMCTextual.text = "Bajo de peso";
+        imagen.image = UIImage(named: "Desnutricion");
+        IMCTextual.textColor = UIColor(red: 146/255, green: 188/255, blue: 212/255, alpha: 1);
+      case 18.5...24.9:
+        IMCTextual.text = "Peso saludable";
+        imagen.image = UIImage(named: "Normal");
+        IMCTextual.textColor = UIColor(red: 53/255, green: 206/255, blue: 150/255, alpha: 1);
+      case 24.9...29.9:
+        IMCTextual.text = "Sobrepeso";
+        imagen.image = UIImage(named: "Obesidad");
+        IMCTextual.textColor = UIColor(red: 252/255, green: 230/255, blue: 0, alpha: 1);
+      case 29.9...34.9:
+        IMCTextual.text = "Obesidad de grado 1";
+        imagen.image = UIImage(named: "Obesidad");
+        IMCTextual.textColor = UIColor(red: 252/255, green: 230/255, blue: 0, alpha: 1);
+      case 34.9...40:
+        IMCTextual.text = "Obesidad de grado 2";
+        imagen.image = UIImage(named: "Obesidad");
+        IMCTextual.textColor = UIColor(red: 252/255, green: 230/255, blue: 0, alpha: 1);
+      default:
+        IMCTextual.text = "Obesidad morbida";
+        imagen.image = UIImage(named: "Obesidad");
+        IMCTextual.textColor = UIColor(red: 252/255, green: 230/255, blue: 0, alpha: 1);
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  }
 
 }
--- Desarrollo-de-aplicaciones/CalculadoraIMC/CalculadoraIMC/ViewController.swift
+++ Desarrollo-de-aplicaciones/CalculadoraIMC/CalculadoraIMC/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
   
   @IBOutlet weak var peso: UITextField!
@@ -26,78 +26,77 @@ class ViewController: UIViewController {
     labelIMC.text = "";
     salidaIMC.text = "";
     diagnostico.text = "";
+    
+    peso.delegate = self;
+    altura.delegate = self;
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    //let sigVista = segue.destination as! ResultadosViewController;
+    let sigVista = segue.destination as! ResultadosViewController;
+    sigVista.altura = Double(altura.text!) ?? 1.0;
+    sigVista.peso = Double(peso.text!) ?? 1.0;
+  }
+  
+  @IBAction func terminarEditar(_ sender: UITextField) {
+    sender.resignFirstResponder();
+  }
+  
+  
+  @IBAction func toque(_ sender: UITapGestureRecognizer) {
+    peso.resignFirstResponder();
+    altura.resignFirstResponder();
   }
   
   @IBAction func calcularIMC(_ sender: UIButton) {
     let alturaValida = validarAltura(), pesoValido = validarPeso();
     if alturaValida && pesoValido {
-      let pesoD: Double! = Double(peso.text!);
-      let alturaD: Double! = Double(altura.text!);
-      let imc = pesoD / alturaD / alturaD;
-      labelIMC.text = "IMC";
-      salidaIMC.text = String(imc);
-      switch imc {
-        case 0...18.5:
-          diagnostico.text = "Bajo de peso";
-        case 18.5...24.9:
-          diagnostico.text = "Peso saludable";
-        case 24.9...29.9:
-          diagnostico.text = "Sobrepeso";
-        case 29.9...34.9:
-          diagnostico.text = "Obesidad de grado 1";
-        case 34.9...40:
-          diagnostico.text = "Obesidad de grado 2";
-        default:
-          diagnostico.text = "Obesidad morbida";
-      }
+      
     }
   }
   
   func validarAltura() -> Bool {
+    let mensajesError = ["La altura es demasiado grande", "La altura es muy pequeña", "La altura debe ser un numero", "Tienes que especificar una altura"];
     var error = "";
     if let altura = self.altura.text {
       let alturaDouble = Double(altura);
       if let aux = alturaDouble {
         if aux > 2.72 {
-          error = "La altura es demasiado grande";
+          error = mensajesError[0];
         } else if(aux < 1) {
-          error = "La altura es muy pequeña"
+          error = mensajesError[1];
         } else {
           return true;
         }
       } else {
-        error = "La altura debe ser un numero";
+        error = mensajesError[2];
       }
     } else {
-      error = "Tienes que especificar una altura"
+      error = mensajesError[3];
     }
-    errorAltura.text = error;
+    //errorAltura.text = error;
     return false;
   }
   
   func validarPeso() -> Bool {
+    let mensajesError = ["El peso es demasiado grande o consigue a un médico", "El peso es muy pequeño", "El peso debe ser un numero", "Tienes que especificar un peso"];
     var error = "";
     if let peso = self.peso.text {
       let pesoDouble = Double(peso);
       if let aux = pesoDouble {
         if aux > 250 {
-          error = "El peso es demasiado grande o consigue a un médico";
+          error = mensajesError[0];
         } else if aux < 25 {
-          error = "El peso es muy pequeño";
+          error = mensajesError[1];
         } else {
           return true;
         }
       } else {
-        error = "El peso debe ser un numero";
+        error = mensajesError[2];
       }
     } else {
-      error = "Tienes que especificar un peso"
+      error = mensajesError[3];
     }
-    errorPeso.text = error;
+    //errorPeso.text = error;
     return false;
   }
   

781db9e672f143f259ca32ad8b2e2fc21ee3ae67
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-06-04 01:03:32 -0500

    Obtener y contar sólo síntomas COVID

--- Lenguajes-de-programacion/challenge_covid.pl
+++ Lenguajes-de-programacion/challenge_covid.pl
@@ -1,6 +1,6 @@
 es_covid(nausea).
 es_covid(diarrea).
-es_covid(dolorPecho).
+es_covid(dolor_pecho).
 es_covid(dificultad_respirar).
 es_covid(perdida_olfato).
 es_covid(decoloracion_dedos).
@@ -26,17 +26,19 @@ es_resfriado(congestion_nasal).
 es_resfriado(cuerpo_cortado).
 es_resfriado(dolor_de_cabeza).
 
-nausea(covid).
-
-diarrea(covid).
-
-dolorPecho(covid).
+dolor_pecho(covid).
 
 dificultad_respirar(covid).
 
 perdida_olfato(covid).
 
 decoloracion_dedos(covid).
+
+fatiga(covid).
+fatiga(influenza).
+
+congestion_nasal(influenza).
+congestion_nasal(resfriado).
 
 tos(covid).
 tos(influenza).
@@ -54,12 +56,6 @@ cuerpo_cortado(covid).
 cuerpo_cortado(influenza).
 cuerpo_cortado(resfriado).
 
-fatiga(covid).
-fatiga(influenza).
-
-congestion_nasal(influenza).
-congestion_nasal(resfriado).
-
 dolor_de_cabeza(influenza).
 dolor_de_cabeza(resfriado).
 dolor_de_cabeza(covid).
@@ -67,12 +63,14 @@ dolor_de_cabeza(covid).
 % Reglas
 
 % COVID 19
-covid([], R):-write(R).
-covid([H|T], R):-
-    (es_covid(H)->
-        Aux is R + 1;
-        Aux is R),
-    covid(T, Aux).
+covid([], COVID):-COVID is 0.
+covid([H|T], COVID):-
+    covid(T, Aux),
+   % (es_covid(H)->  
+      COVID is Aux + 1
+      %write(" "))
+    %COVID is Aux,
+    .
 
 % Influenza
 influenza([], R):-write(R).
@@ -89,3 +87,20 @@ resfriado([H|T], R):-
         Aux is R + 1;
         Aux is R),
     resfriado(T, Aux).
+
+add_sintoma(Sintomas):-read(S),add_sintoma(S, Sintomas).
+add_sintoma(listo, []):-!.
+add_sintoma(S, [S | Sintomas]):-read(NS), add_sintoma(NS, Sintomas).
+
+imprimir_sintomas([H|T]):-write(H), nl, imprimir_sintomas(T).
+imprimir_sintomas([]):-!.
+
+diagnosticar():-
+  write("Ingrese su nombre:"),nl,
+  write("De la lista de síntomas que mostraremos a continuación, escriba los que apliquen a su caso."), nl,
+  write("Cuando haya escrito todos sus sintomas escriba: listo"), nl,
+  imprimir_sintomas([dolor_pecho, dificultad_respirar, perdida_olfato]),
+  add_sintoma(Sintomas),
+  write(Sintomas),nl,
+  covid(Sintomas, COVID),
+  write(COVID).

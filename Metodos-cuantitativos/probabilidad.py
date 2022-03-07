4a12472d930e88f5590c209ee31010dccf077dc5
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-03-07 00:26:48 -0600

    Binomial negativa y Poisson

--- Metodos-cuantitativos/probabilidad.py
+++ Metodos-cuantitativos/probabilidad.py
@@ -8,3 +8,10 @@ def combinaciones(n, x):
 def binomial(x, n, p):
     q = 1 - p
     return combinaciones(n, x) * p**x * q**(n-x)
+
+
+def poisson(x, mu):
+    return math.exp(-mu) * mu**x / factorial(x)
+
+def binomial_negativa(x, r, p):
+    return combinaciones(x+r-1, r-1) * p**r * (1-p)**x

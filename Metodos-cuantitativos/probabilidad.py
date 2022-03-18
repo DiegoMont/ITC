4843a0569391cdc1f4fc7f8c5db7660475ce8010
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-03-17 19:02:40 -0600

    Parcial 1 Leopold

--- Metodos-cuantitativos/probabilidad.py
+++ Metodos-cuantitativos/probabilidad.py
@@ -4,7 +4,6 @@ def factorial(n):
 def combinaciones(n, x):
     return factorial(n) / factorial(x) / factorial(n-x)
 
-
 def binomial(x, n, p):
     q = 1 - p
     return combinaciones(n, x) * p**x * q**(n-x)
@@ -15,3 +14,24 @@ def poisson(x, mu):
 
 def binomial_negativa(x, r, p):
     return combinaciones(x+r-1, r-1) * p**r * (1-p)**x
+
+def p(x):
+    probabilidad = [1/8, 3/8, 3/8, 1/8]
+    return probabilidad[x]
+
+def media(i, n):
+    mu = 0
+    while i <= n:
+        mu += p(i) * i
+        i += 1
+    return mu
+
+def varianza(i, n):
+    s = 0
+    mu = media(i, n)
+    while i <= n:
+        s += (i - mu)**2 * p(i)
+        i += 1
+    return s
+
+print(1-binomial(0, 4, 0.8))

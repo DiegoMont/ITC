eaf84fdcb78511cafb930e6bebc0584e4c10393c
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-04-20 21:51:59 -0500

    Calculator class

---
+++ Arquitectura-Software/calculator/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        double result;
+        result = calc.divide(1, 0);
+        System.out.println(result);
+    }
+}

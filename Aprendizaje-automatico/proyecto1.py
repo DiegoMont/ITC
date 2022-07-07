131543d819e2d38a541ab5bf7c302d3d675aff6a
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-07-07 11:19:56 -0500

    Testing with fold split

--- Aprendizaje-automatico/proyecto1.py
+++ Aprendizaje-automatico/proyecto1.py
@@ -1,14 +1,44 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, StratifiedKFold, train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier, export_text
 
 
-DATASET_SPLIT_SEED = 40692
+DATASET_SPLIT_SEED = 4069
 TREE_CLASSIFIER = DecisionTreeClassifier()
 NAIVE_BAYES_CLASSIFIER = MultinomialNB()
+SPLIT_RATIOS = [0.2, 0.25, 0.3, 1/3, 0.5]
+FOLDS = [2, 3, 5, 7, 10]
 
+
+def split_test_evaluate():
+    test_id = 1
+    for split_ratio in SPLIT_RATIOS:
+        (training_data, test_data, training_classes, test_classes) = get_splitted_data(data, classes, split_ratio)
+        print(f"==== TEST {test_id} ====")
+        print(f"Training data: {(1 - split_ratio) * 100}    Test data: {(split_ratio * 100)}")
+        build_and_evaluate_models(training_data, test_data, training_classes, test_classes)
+        test_id += 1
+
+def cross_validation_evaluate():
+    test_id = 1
+    for fold in FOLDS:
+        (training_data, test_data, training_classes, test_classes) = get_fold_splitted_data(data, classes, fold)
+        print(f"==== TEST {test_id} ====")
+        print("Folds: " + str(fold))
+        build_and_evaluate_models(training_data, test_data, training_classes, test_classes)
+        test_id += 1
+
+def build_and_evaluate_models(training_data, test_data, training_classes, test_classes):
+    naive_bayes_model = NAIVE_BAYES_CLASSIFIER.fit(training_data, training_classes)
+    decision_tree_model = TREE_CLASSIFIER.fit(training_data, training_classes) 
+    print("Decision tree report")
+    tree_text = export_text(decision_tree_model, feature_names=colnames)
+    # print(tree_text)
+    print_model_evaluation(decision_tree_model, test_data, test_classes, class_values)
+    print("Naive Bayes report")
+    print_model_evaluation(naive_bayes_model, test_data, test_classes, class_values)
 
 def load_dataset():
     data = pd.read_csv("car.data")
@@ -33,6 +63,13 @@ def get_splitted_data(data, classes, train_data_ratio):
         random_state=DATASET_SPLIT_SEED
     )
 
+def get_fold_splitted_data(data, classes, fold):
+    skf = KFold(fold, shuffle=False)
+    for train_index, test_index in skf.split(data, classes):
+        training_data, test_data = data.iloc[train_index], data.iloc[test_index]
+        training_classes, test_classes = classes[train_index], classes[test_index]
+    return (training_data, test_data, training_classes, test_classes)
+
 def print_model_evaluation(model, test_data, test_classes, class_values):
     predictions = model.predict(test_data)
     accuracy = accuracy_score(test_classes, predictions)
@@ -44,18 +81,5 @@ def print_model_evaluation(model, test_data, test_classes, class_values):
 
 
 (data, classes, colnames, class_values) = load_dataset()
-split_ratios = [0.2, 0.25, 0.3, 1/3, 0.5]
-test_id = 1
-for split_ratio in split_ratios:
-    (training_data, test_data, training_classes, test_classes) = get_splitted_data(data, classes, split_ratio)
-    naive_bayes_model = NAIVE_BAYES_CLASSIFIER.fit(training_data, training_classes)
-    decision_tree_model = TREE_CLASSIFIER.fit(training_data, training_classes) 
-    print(f"==== TEST {test_id} ====")
-    print(f"Training data: {(1 - split_ratio) * 100}    Test data: {(split_ratio * 100)}")
-    print("Decision tree report")
-    tree_text = export_text(decision_tree_model, feature_names=colnames)
-    # print(tree_text)
-    print_model_evaluation(decision_tree_model, test_data, test_classes, class_values)
-    print("Naive Bayes report")
-    print_model_evaluation(naive_bayes_model, test_data, test_classes, class_values)
-    test_id += 1
+split_test_evaluate()
+cross_validation_evaluate()
